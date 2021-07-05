beac622799cfa9f4ab74b2473f39419a53fe5fe9
pattonjp <pattonjp@gmail.com>
2021-07-05 13:46:02 -0500

    removing node

--- node/install.sh
+++
@@ -1,6 +0,0 @@
-# if test ! $(which spoof)
-# then
-#   sudo npm install spoof -g
-# fi
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
--- node/nvm.zsh
+++
@@ -1,8 +0,0 @@
-# declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-
-# NODE_GLOBALS+=("node")
-# NODE_GLOBALS+=("nvm")
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --silent --no-use  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
